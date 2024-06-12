c2fffca2f696616edf58eb7cec975f0959eb4b60
Andrew Carolan <andycarolan9@gmail.com>
2024-06-12 22:18:13 +0100

    Updated Set Layer Text to work for any text layer

--- Announcement/Announcement.py
+++ Announcement/Announcement.py
@@ -6,27 +6,37 @@ from gimpfu import *
 import sys
 sys.stderr = open( 'c:\\temp\\gimpstderr.txt', 'a')
 
+#Constants
 PIXEL_UNIT = 0
 COLOR_WHITE = (255, 255, 255)
 FONT_NAME = "Arial Bold Italic"
 
-def announcement_automation(image, active_layer):
-    set_competition_title(image)
+# Layer Names
+COMPETITION_LAYER_NAME = "Competition"
+DATE_LAYER_NAME = "Date"
+TIME_LAYER_NAME = "Time"
+VENUE_LAYER_NAME = "Venue"
+HOME_TEAM_LAYER_NAME = "HomeTeam"
+AWAY_TEAM_LAYER_NAME = "AwayTeam"
 
-def set_competition_title(image):
-    layer_name = "Competition" #TODO: Competition should not be hard coded here. Create a data base of layer id and layer name.
-    layer = find_layer(image.layers, layer_name) 
-    if layer is None:
-        pdb.gimp_message("Failed to find layer with name " + layer_name)
-        return
-    set_layer_text(layer, "Test Title", 65)
+def announcement_automation(image, active_layer):
+    set_layer_text(image, COMPETITION_LAYER_NAME, "Title", 65)
+    set_layer_text(image, DATE_LAYER_NAME, "Monday 12th June", 32)
+    set_layer_text(image, TIME_LAYER_NAME, "7:30 PM", 32)
+    set_layer_text(image, VENUE_LAYER_NAME, "Home", 32)
+    set_layer_text(image, HOME_TEAM_LAYER_NAME, "Glen Emmets", 32)
+    set_layer_text(image, AWAY_TEAM_LAYER_NAME, "Away Team", 32)
 
 def find_layer(layers, layer_name):
     for layer in layers:
         if layer.name == layer_name:
             return layer
 
-def set_layer_text(layer, text, font_size):
+def set_layer_text(image, layer_name, text, font_size):
+    layer = find_layer(image.layers, layer_name) 
+    if layer is None:
+        pdb.gimp_message("Failed to set text in layer. Could not find layer with name " + layer_name)
+        return
     #TODO: This should get the font, size & color value from the layer to copy. Need method to get info from layer markup as get methods do not work correctly
     pdb.gimp_text_layer_set_text(layer, text)
     pdb.gimp_text_layer_set_color(layer, COLOR_WHITE)
