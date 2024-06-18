ed341864f64f3e1f65a1d6614d4a1264ae64fb2f
Andrew Carolan <andycarolan9@gmail.com>
2024-06-19 00:00:27 +0100

    Added align centre method

--- Announcement/Announcement.py
+++ Announcement/Announcement.py
@@ -32,16 +32,29 @@ def find_layer(layers, layer_name):
         if layer.name == layer_name:
             return layer
 
+def align_layer_centre(layer, original_x_offset, original_y_offset, original_width, original_height):
+    original_center_x = original_x_offset + (original_width / 2)
+    original_center_y = original_y_offset + (original_height / 2)
+    layer_half_width = layer.width / 2
+    layer_half_height = layer.height / 2
+    new_x_offset = original_center_x - layer_half_width
+    new_y_offset = original_center_y - layer_half_height
+    layer.set_offsets(new_x_offset, new_y_offset)
+
 def set_layer_text(image, layer_name, text, font_size):
     layer = find_layer(image.layers, layer_name) 
     if layer is None:
         pdb.gimp_message("Failed to set text in layer. Could not find layer with name " + layer_name)
         return
     #TODO: This should get the font, size & color value from the layer to copy. Need method to get info from layer markup as get methods do not work correctly
+    xOffset, yOffset = layer.offsets
+    original_width = layer.width
+    original_height = layer.height
     pdb.gimp_text_layer_set_text(layer, text)
     pdb.gimp_text_layer_set_color(layer, COLOR_WHITE)
     pdb.gimp_text_layer_set_font_size(layer, font_size, PIXEL_UNIT)
     pdb.gimp_text_layer_set_font(layer, FONT_NAME)
+    align_layer_centre(layer, xOffset, yOffset, original_width, original_height)
 
 register(
           "python_fu_Announcement",
